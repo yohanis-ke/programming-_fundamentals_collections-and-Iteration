d39f191b0b3478a23bf9bba4a051197c416cff49
yohanis-ke <yohanes.ke@gmail.com>
2018-07-20 20:29:40 -0400

    I just finished assignment nmber 10.1 & 10.2

---
+++ exercise9.rb
@@ -0,0 +1,14 @@
+
+
+
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+#The fanction to display the hash elements with some extra words
+# def display_list(student)
+#   student.each {|key, value| puts "#{key} #{value} students" }
+# end
+#
+# display_list(students)
